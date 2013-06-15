0405fb2c8df21c0732cc642b89e42e505f7f9c27
Maximilian Tagher <feedback.tagher@gmail.com>
2013-06-15 07:27:04 -0700

    initial commit

--- TestFCunction/main.m
+++ TestFCunction/main.m
@@ -8,6 +8,9 @@
 
 #import <Foundation/Foundation.h>
 
+int emptyParens();
+int voidParens(void);
+
 int main(int argc, const char * argv[])
 {
 
@@ -15,8 +18,23 @@ int main(int argc, const char * argv[])
         
         // insert code here...
         NSLog(@"Hello, World!");
-        
+        emptyParens();
+        emptyParens(1);
+        voidParens();
+        voidParens(1);
     }
     return 0;
+}
+
+int emptyParens(void)
+{
+    NSLog(@"Empty parens called");
+    return 1;
+}
+
+int voidParens(void)
+{
+    NSLog(@"Void parens called");
+    return 1;
 }
 
