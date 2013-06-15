224163f488579e314b768610f0b7b6542c5f69e9
Maximilian Tagher <feedback.tagher@gmail.com>
2013-06-15 07:22:11 -0700

    Initial Commit

---
+++ TestFCunction/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TestFCunction
+//
+//  Created by Maximilian Tagher on 6/15/13.
+//  Copyright (c) 2013 Tagher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
